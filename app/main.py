e54dd84e8d5c7546a155ef710faf4c93b8aa305a
vincent porte <vincent@neuralia.co>
2021-06-15 17:46:57 +0200

    quick fix

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
-# main.py
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/public", StaticFiles(directory="public"), name="public")
+app.mount("/static", StaticFiles(directory="public"), name="public")
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="app/templates")
 
-@app.get("/health")
-async def health():
-    return {"message": "I'm alive"}
 
 @app.get("/")
 async def root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request
+        })
