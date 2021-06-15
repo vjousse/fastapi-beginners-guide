48e6031887587b7a5c3bdf8d798b95f2616c62f0
Vincent Jousse <vincent@jousse.org>
2021-06-15 14:24:32 +0100

    Add home template

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="public"), name="public")
+
+templates = Jinja2Templates(directory="app/templates")
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request
+        })
