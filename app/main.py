8a0871cfc226c8b80fa432b5bb0d49dfb9b87f08
Vincent Jousse <vincent@jousse.org>
2021-06-15 19:40:34 +0100

    Use /public not /static

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="public"), name="public")
+app.mount("/public", StaticFiles(directory="public"), name="public")
 
 templates = Jinja2Templates(directory="app/templates")
 
