48f535f050ec0086184f83ac78b18a095d55d007
vincent porte <vincent@neuralia.co>
2021-06-22 11:48:22 +0200

    tortoise patch

--- app/main.py
+++ app/main.py
@@ -5,6 +5,14 @@ from tortoise import fields
 from tortoise.models import Model
 from tortoise.contrib.fastapi import register_tortoise
 
+
+# tortoise logging patch
+import logging
+from tortoise.contrib import fastapi
+fastapi.logging = logging.getLogger('uvicorn')
+# end of patch
+
+
 app = FastAPI()
 
 app.mount("/public", StaticFiles(directory="public"), name="public")
