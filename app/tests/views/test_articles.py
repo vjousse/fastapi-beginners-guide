40f87a32473345b5e88b5fc44c78cf9c5df29157
Vincent Jousse <vincent@jousse.org>
2021-08-23 14:28:24 +0100

    Move random article creation as a fixture

--- app/tests/views/test_articles.py
+++ app/tests/views/test_articles.py
@@ -1,17 +1,13 @@
-import asyncio
 from fastapi.testclient import TestClient
-
-from app.tests.utils.article import create_random_article
+from app.models.article import Article
 
 
 def test_read_item(client: TestClient,
-                   event_loop: asyncio.AbstractEventLoop) -> None:
-
-    article = event_loop.run_until_complete(create_random_article())
+                   random_article: Article) -> None:
 
     response = client.get("api/articles")
     assert response.status_code == 200
     content = response.json()
     first_article = content[0]
-    assert first_article["title"] == article.title
-    assert first_article["content"] == article.content
+    assert first_article["title"] == random_article.title
+    assert first_article["content"] == random_article.content
