7740d7474dc4e38509ca816d5fd37b9daadc77c6
Vincent Jousse <vincent@jousse.org>
2023-01-07 12:39:26 +0100

    Fix tests for new starlette version
    See https://github.com/tortoise/tortoise-orm/issues/1029

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,21 +1,29 @@
+import asyncio
 import os
+from typing import Generator, Iterator
+
 import pytest
-from tortoise.contrib.test import finalizer, initializer
-from app.core.config import settings
-from typing import Generator
 from fastapi.testclient import TestClient
+from tortoise.contrib.test import finalizer, initializer
+
+from app.core.config import settings
 from app.main import app
 
 
 @pytest.fixture(scope="module")
-def client() -> Generator:
+def event_loop() -> Iterator[asyncio.AbstractEventLoop]:
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+# Test client
+@pytest.fixture(scope="module")
+def client(event_loop: asyncio.BaseEventLoop) -> Iterator[TestClient]:
     db_url = os.environ.get("TORTOISE_TEST_DB", "sqlite://:memory:")
-    initializer(settings.TORTOISE_MODELS, db_url=db_url, app_label="models")
+    initializer(
+        settings.TORTOISE_MODELS, db_url=db_url, app_label="models", loop=event_loop
+    )
     with TestClient(app) as c:
         yield c
     finalizer()
-
-
-@pytest.fixture(scope="module")
-def event_loop(client: TestClient) -> Generator:
-    yield client.task.get_loop()
