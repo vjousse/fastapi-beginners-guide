3f82377d821f8c66c6c55d7baaa201df1cf578f8
Vincent Jousse <vincent@jousse.org>
2024-02-09 18:07:18 +0100

    Use SQLAlchemy instead of Tortoise

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from tortoise import fields
-from tortoise.models import Model
-from tortoise.contrib.fastapi import register_tortoise
+from sqlalchemy import Column, DateTime, Integer, String, create_engine, select
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.sql import func
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
 
 app = FastAPI()
 
@@ -11,71 +22,64 @@ app.mount("/public", StaticFiles(directory="public"), name="public")
 
 templates = Jinja2Templates(directory="app/templates")
 
-register_tortoise(
-    app,
-    db_url="sqlite://db.sqlite3",
-    modules={"models": ["app.main"]},
-    generate_schemas=True,
-    add_exception_handlers=True,
-)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-class Article(Model):
+class Article(Base):
+    __tablename__ = "articles"
 
-    id = fields.IntField(pk=True)
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(String)
 
-    title = fields.TextField()
-    content = fields.TextField()
-
-    created_at = fields.DatetimeField(auto_now_add=True)
-    updated_at = fields.DatetimeField(auto_now=True)
+    created_at = Column(String, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
     def __str__(self):
         return self.title
 
 
-@app.get("/articles/create", include_in_schema=False)
-async def articles_create(request: Request):
+Base.metadata.create_all(bind=engine)
 
-    article = await Article.create(
-        title="Mon titre de test",
-        content="Un peu de contenu<br />avec deux lignes"
+
+@app.get("/articles/create", include_in_schema=False)
+async def articles_create(request: Request, db: Session = Depends(get_db)):
+    article = Article(
+        title="Mon titre de test", content="Un peu de contenu<br />avec deux lignes"
     )
+    db.add(article)
+    db.commit()
+    db.refresh(article)
 
     return templates.TemplateResponse(
-        "articles_create.html",
-        {
-            "request": request,
-            "article": article
-        })
+        "articles_create.html", {"request": request, "article": article}
+    )
 
 
 @app.get("/articles", include_in_schema=False)
-async def articles_list(request: Request):
-
-    articles = await Article.all().order_by('created_at')
+async def articles_list(request: Request, db: Session = Depends(get_db)):
+    articles_statement = select(Article)
+    articles = db.scalars(articles_statement).all()
 
     return templates.TemplateResponse(
-        "articles_list.html",
-        {
-            "request": request,
-            "articles": articles
-        })
+        "articles_list.html", {"request": request, "articles": articles}
+    )
 
 
 @app.get("/api/articles")
-async def api_articles_list():
+async def api_articles_list(db: Session = Depends(get_db)):
+    articles_statement = select(Article).order_by(Article.created_at)
 
-    articles = await Article.all().order_by('created_at')
-
-    return articles
+    return db.scalars(articles_statement).all()
 
 
 @app.get("/", include_in_schema=False)
 async def root(request: Request):
-
-    return templates.TemplateResponse(
-        "home.html",
-        {
-            "request": request
-        })
+    return templates.TemplateResponse("home.html", {"request": request})
